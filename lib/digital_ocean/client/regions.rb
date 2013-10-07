29d7fe6dda15ac94247e475048e3cd2ad97ff7c1
Casey Scarborough <caseyscarborough@gmail.com>
2013-10-07 19:04:59 -0400

    Update doc for regions API.

--- lib/digital_ocean/client/regions.rb
+++ lib/digital_ocean/client/regions.rb
@@ -4,11 +4,15 @@ module DigitalOcean
     # Methods for the regions API.
     module Regions
 
+      # All Regions
+      # 
+      # This method will return all the available regions within the DigitalOcean cloud.
+      # @return [Hashie::Mash] A hash of regions
+      # @example
+      #   client.regions
       def regions
         get "/regions"
       end
-
-
 
     end
 
